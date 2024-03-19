96206dd660a93a267c2770a5f18be4b3113c2f90
snehasoni26 <snehavida26@gmail.com>
2024-03-19 12:06:16 +0530

    Update DoorLock.ino

--- DoorLock.ino
+++ DoorLock.ino
@@ -48,4 +48,69 @@ void sendMessage()
 {
     Blynk.logEvent("notification", "Theft Alert!");
 }
+BLYNK_WRITE(V0)
+{
+  int pinValue = param.asInt(); 
+  // 0-> Default
+  // 1-> Door Open
+  // 2-> Door Closed
+  if (pinValue == 2)
+  {
+    lockDoor();
+    lcd.clear();
+    lcd.setCursor(0,1);
+    lcd.print("Burglar Detected");
+    sendMessage();
+    soundBuzzer();
+  }
+  else if(pinValue==1) 
+  {
+    reset();
+    lcd.clear();
+    unlockDoor();
+    noTone(buz_pin);
+    lcd.setCursor(0,1);
+    lcd.print("Welcome");
+  }
+  else if(pinValue==0)
+  {
+    reset();
+  }
+}
+
+void setup()
+{
+  Blynk.begin(auth, ssid, pass);
+  s.attach(servo_pin); 
+  pinMode(buz_pin,OUTPUT);
+  lcd.begin(16,2);
+  reset();
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
